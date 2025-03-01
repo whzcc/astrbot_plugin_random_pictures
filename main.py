2fcc130ec9e339afa6bec49039e863b9853461a6
whzcc <74447737+whzcc@users.noreply.github.com>
2025-03-01 22:06:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api.all import *
+import os,json
+import random
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
-class MyPlugin(Star):
+
+@register("random-pictures", "whzc", "随机发一些图片", "1.0.0", "repo url")
+
+class Main(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    # 插件目录下的图片
+        try:
+            os.makedir("pictures")
+            logger.info("成功为随机图片创建文件夹")
+        except:
+            logger.info("本次未为随机图片创建文件夹")
+        global pictures_dir
+        pictures_dir = os.path.dirname(os.path.abspath(__file__))+"\\pictures"  # 获取图片所在目录
+        files = os.listdir(pictures_dir)   # 读入文件夹
+        global num_png
+        num_png = len(files)       # 统计文件夹中的文件个数
+        logger.info(f"将在{pictures_dir}中寻找{num_png}张图片")
+
+@event_message_type(EventMessageType.PRIVATE_MESSAGE)
+async def random_pictures(self, event: AstrMessageEvent):
+    message_str = event.message_str # 获取消息的纯文本内容
+    if message_str.startswith("/抽明信片"):
+
+        # 随机获取图片文件的路径
+        i = random.randint(1,num_png)
+        pictures_file = pictures_dir+f"\\{i}.jpg" # 发送图片
+        sender_id = event.get_sender_id()
+
+        # 获取图片的解释说明
+        with open(pictures_dir+"\\info.json",encoding='utf-8') as f:
+            info = json.load(f)[str(i)] # 读到的解释说明
+
+        chain = [
+            Plain(f"Agrato为你抽取到了{i}号图片"),
+            Image.fromFileSystem(pictures_file), # 从本地文件目录发送图片
+            Plain(info)
+        ]
+        yield event.chain_result(chain)
+
+        event.stop_event()
+
